d6d82390b3eff601b216eca9986d5af4c617113c
rezvan shokrani <rezvanshokrani@gmail.com>
2021-04-15 08:58:34 -0700

    add featureextraction

---
+++ featureextraction/comet/utils/rezvan.py
@@ -0,0 +1,9 @@
+# import sys
+# print(sys.path)
+# sys.path.insert(0, "e:\\course\\Papers\\DeCOCO\\featureextraction\\comet\\utils1")
+# print(sys.path)
+
+from utils import DD, aa
+
+b = aa()
+b.a(ss=70)
